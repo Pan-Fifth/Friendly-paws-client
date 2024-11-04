345115416b800970c9f8dcf54828f260535c3899
Sarunyamk <Sarunya46mk@gmail.com>
2024-11-04 13:53:50 +0700

    feat : edit layout

--- src/components/admin-components/ReportEvent.jsx
+++ src/components/admin-components/ReportEvent.jsx
@@ -56,87 +56,94 @@ export default function ReportEvent() {
 
     return (
         <div className="container mx-auto px-4 py-8">
-            <div className="flex gap-4 mb-6">
-                <input
-                    type="date"
-                    value={startDate}
-                    onChange={e => setStartDate(e.target.value)}
-                    className="px-4 py-2 border rounded-lg focus:ring-2 focus:ring-blue-500"
-                />
-                <input
-                    type="date"
-                    value={endDate}
-                    onChange={e => setEndDate(e.target.value)}
-                    className="px-4 py-2 border rounded-lg focus:ring-2 focus:ring-blue-500"
-                />
-                <button
-                    onClick={handleFetchReport}
-                    className="bg-blue-600 text-white px-6 py-2 rounded-lg hover:bg-blue-700 transition duration-200"
-                >
-                    รายงาน
-                </button>
-                <button
-                    onClick={handleFetchAllReport}
-                    className="bg-blue-600 text-white px-6 py-2 rounded-lg hover:bg-blue-700 transition duration-200"
-                >
-                    กิจกรรมทั้งหมดของปีนี้
-                </button>
-                <button
-                    onClick={handleExportExcel}
-                    className="bg-blue-600 text-white px-6 py-2 rounded-lg hover:bg-blue-700 transition duration-200"
-                >
-                    บันทึกข้อมูล
-                </button>
+            <div className="flex justify-between gap-4 mb-6">
+                <div className='flex gap-6'>
+                    <p className='flex justify-center items-center'>วันที่เริ่มกิจกรรม : </p>
+                    <input
+                        type="date"
+                        value={startDate}
+                        onChange={e => setStartDate(e.target.value)}
+                        className="px-4 py-2 border rounded-lg focus:ring-2 focus:ring-blue-500"
+                    />
+                    <input
+                        type="date"
+                        value={endDate}
+                        onChange={e => setEndDate(e.target.value)}
+                        className="px-4 py-2 border rounded-lg focus:ring-2 focus:ring-blue-500"
+                    />
+                    <button
+                        onClick={handleFetchReport}
+                        className="bg-blue-600 text-white px-6 py-2 rounded-lg hover:bg-blue-700 transition duration-200"
+                    >
+                        รายงาน
+                    </button>
+                </div>
+                <div className=' flex gap-6'>
+                    <button
+                        onClick={handleFetchAllReport}
+                        className="bg-blue-600 text-white px-6 py-2 rounded-lg hover:bg-blue-700 transition duration-200"
+                    >
+                        กิจกรรมทั้งหมดของปีนี้
+                    </button>
+                    <button
+                        onClick={handleExportExcel}
+                        className="bg-blue-600 text-white px-6 py-2 rounded-lg hover:bg-blue-700 transition duration-200"
+                    >
+                        บันทึกข้อมูล
+                    </button>
+                </div>
             </div>
 
-            {events && events.length > 0 ? (
-                <div className="overflow-x-auto shadow-lg rounded-lg">
-                    <table className="min-w-full bg-white">
-                        <thead className="bg-gray-100">
-                            <tr>
-                                <th className="px-6 py-3 border-b text-left text-xs font-semibold text-gray-600 uppercase">อีเวนท์ไอดี</th>
-                                <th className="px-6 py-3 border-b text-left text-xs font-semibold text-gray-600 uppercase">ชื่อกิจกรรม</th>
-                                <th className="px-6 py-3 border-b text-left text-xs font-semibold text-gray-600 uppercase">รายละเอียด</th>
-                                <th className="px-6 py-3 border-b text-left text-xs font-semibold text-gray-600 uppercase">วันที่เริ่มงาน</th>
-                                <th className="px-6 py-3 border-b text-left text-xs font-semibold text-gray-600 uppercase">วันที่จบงาน</th>
-                                <th className="px-6 py-3 border-b text-left text-xs font-semibold text-gray-600 uppercase">สถานที่จัดงาน</th>
-                                <th className="px-6 py-3 border-b text-left text-xs font-semibold text-gray-600 uppercase">สถานะ</th>
-                                <th className="px-6 py-3 border-b text-left text-xs font-semibold text-gray-600 uppercase">วันที่สร้าง</th>
-                            </tr>
-                        </thead>
-                        <tbody className="divide-y divide-gray-200">
-                            {events.map(event => (
-                                <tr key={event.id} className="hover:bg-gray-50">
-                                    <td className="px-6 py-4 whitespace-nowrap">{event.id}</td>
-                                    <td className="px-6 py-4 whitespace-nowrap">{event.title_th}</td>
-                                    <td className="px-6 py-4">{event.description_th}</td>
-                                    <td className="px-6 py-4">{new Date(event.date_start).toLocaleDateString()}</td>
-                                    <td className="px-6 py-4">{new Date(event.date_end).toLocaleDateString()}</td>
-                                    <td className="px-6 py-4">{event.location}</td>
-                                    <td className={`px-6 py-4 ${event.status === 'PENDING'
-                                        ? 'text-yellow-500'
-                                        : event.status === 'CANCELLED'
-                                            ? 'text-red-500'
-                                            : event.status === 'ACTIVE'
-                                                ? 'text-green-500'
-                                                : event.status === 'COMPLETED'
-                                                    ? 'text-blue-500'
-                                                    : ''
-                                        }`}>{event.status}</td>
-                                    <td className="px-6 py-4">{new Date(event.created_at).toLocaleDateString()}</td>
-
+            {
+                events && events.length > 0 ? (
+                    <div className="overflow-x-auto shadow-lg rounded-lg">
+                        <table className="min-w-full bg-white">
+                            <thead className="bg-gray-100">
+                                <tr>
+                                    <th className="px-6 py-3 border-b text-left text-xs font-semibold text-gray-600 uppercase">อีเวนท์ไอดี</th>
+                                    <th className="px-6 py-3 border-b text-left text-xs font-semibold text-gray-600 uppercase">ชื่อกิจกรรม</th>
+                                    <th className="px-6 py-3 border-b text-left text-xs font-semibold text-gray-600 uppercase">รายละเอียด</th>
+                                    <th className="px-6 py-3 border-b text-left text-xs font-semibold text-gray-600 uppercase">วันที่เริ่มงาน</th>
+                                    <th className="px-6 py-3 border-b text-left text-xs font-semibold text-gray-600 uppercase">วันที่จบงาน</th>
+                                    <th className="px-6 py-3 border-b text-left text-xs font-semibold text-gray-600 uppercase">สถานที่จัดงาน</th>
+                                    <th className="px-6 py-3 border-b text-left text-xs font-semibold text-gray-600 uppercase">สถานะ</th>
+                                    <th className="px-6 py-3 border-b text-left text-xs font-semibold text-gray-600 uppercase">วันที่สร้าง</th>
                                 </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
-            )
-                : (
-                    <div className="bg-gray-800 h-96 mt-20 flex justify-center items-center">
-                        <p className="text-gray-500">ไม่พบข้อมูล</p>
+                            </thead>
+                            <tbody className="divide-y divide-gray-200">
+                                {events.map(event => (
+                                    <tr key={event.id} className="hover:bg-gray-50">
+                                        <td className="px-6 py-4 whitespace-nowrap">{event.id}</td>
+                                        <td className="px-6 py-4 whitespace-nowrap">{event.title_th}</td>
+                                        <td className="px-6 py-4">{event.description_th}</td>
+                                        <td className="px-6 py-4">{new Date(event.date_start).toLocaleDateString()}</td>
+                                        <td className="px-6 py-4">{new Date(event.date_end).toLocaleDateString()}</td>
+                                        <td className="px-6 py-4">{event.location}</td>
+                                        <td className={`px-6 py-4 ${event.status === 'PENDING'
+                                            ? 'text-yellow-500'
+                                            : event.status === 'CANCELLED'
+                                                ? 'text-red-500'
+                                                : event.status === 'ACTIVE'
+                                                    ? 'text-green-500'
+                                                    : event.status === 'COMPLETED'
+                                                        ? 'text-blue-500'
+                                                        : ''
+                                            }`}>{event.status}</td>
+                                        <td className="px-6 py-4">{new Date(event.created_at).toLocaleDateString()}</td>
+
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
                     </div>
-                )}
-        </div>
+                )
+                    : (
+                        <div className="bg-gray-800 h-96 mt-20 flex justify-center items-center">
+                            <p className="text-gray-500">ไม่พบข้อมูล</p>
+                        </div>
+                    )
+            }
+        </div >
     )
 }
 
