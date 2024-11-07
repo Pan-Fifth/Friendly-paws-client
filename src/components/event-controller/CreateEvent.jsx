007f272f36d0ab2b50d887ba6ec0ebea7750e46a
Ju-Pa39 <pichai.ju39@gmail.com>
2024-11-07 16:03:23 +0700

    hotfix-loading

--- src/components/event-controller/CreateEvent.jsx
+++ src/components/event-controller/CreateEvent.jsx
@@ -8,6 +8,8 @@ import useAuthStore from '@/src/stores/AuthStore'
 import { useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
 import EventFormValidate from '@/src/utils/EventFormValidate'
+import Lottie from "lottie-react";
+import AnimationDownload from '../../assets/AnimationDownload.json'
 
 export default function CreateEvent() {
     const [formatError, setFormatError] = useState({});
@@ -81,95 +83,114 @@ export default function CreateEvent() {
         }
     }
 
-    return (
-        <form onSubmit={handleSubmit} className="space-y-4 p-6 bg-white shadow-md rounded-md">
-            <div>
-                <Label htmlFor="title_th">ชื่อกิจกรรม</Label>
-                <Input
-                    type="text"
-                    name="title_th"
-                    value={formData.title_th}
-                    onChange={handleInputChange}
-                    placeholder="กรอกชื่อ Event"
-                />
-                {formatError.title_th && <p className="text-red-500 text-xs">{formatError.title_th}</p>}
-            </div>
-            <div>
-                <Label htmlFor="title_en">ชื่อกิจกรรม (ภาษาอังกฤษ)</Label>
-                <Input
-                    type="text"
-                    name="title_en"
-                    value={formData.title_en}
-                    onChange={handleInputChange}
-                    placeholder="กรอกชื่อ Event"
-                />
-                {formatError.title_en && <p className="text-red-500 text-xs">{formatError.title_en}</p>}
-            </div>
 
-            <div>
-                <Label htmlFor="location">สถานที่จัดกิจกรรม</Label>
-                <Input
-                    type="text"
-                    name="location"
-                    value={formData.location}
-                    onChange={handleInputChange}
-                    placeholder="กรอก สถานที่จัดกิจกรรม"
-                />
-                {formatError.location && <p className="text-red-500 text-xs">{formatError.location}</p>}
-            </div>
-            <div>
-                <Label htmlFor="description_th">รายละเอียดกิจกรรม</Label>
-                <Textarea
-                    name="description_th"
-                    value={formData.description_th}
-                    onChange={handleInputChange}
-                    placeholder="กรอกรายละเอียด Event"
-                />
-                {formatError.description_th && <p className="text-red-500 text-xs">{formatError.description_th}</p>}
-            </div>
-            <div>
-                <Label htmlFor="description_en">รายละเอียดกิจกรรม (ภาษาอังกฤษ)</Label>
-                <Textarea
-                    name="description_en"
-                    value={formData.description_en}
-                    onChange={handleInputChange}
-                    placeholder="กรอกรายละเอียด Event"
-                />
-                {formatError.description_en && <p className="text-red-500 text-xs">{formatError.description_en}</p>}
-            </div>
-            <div className="grid grid-cols-2 gap-4">
+    return (
+        <div>
+            <form onSubmit={handleSubmit} className="space-y-4 p-6 bg-white shadow-md rounded-md">
                 <div>
-                    <Label htmlFor="date_start">วันที่เริ่ม</Label>
+                    <Label htmlFor="title_th">ชื่อกิจกรรม</Label>
                     <Input
-                        type="date"
-                        name="date_start"
-                        value={formData.date_start}
-                        onChange={(e) => handleDateChange('date_start', e.target.value)}
+                        type="text"
+                        name="title_th"
+                        value={formData.title_th}
+                        onChange={handleInputChange}
+                        placeholder="กรอกชื่อ Event"
                     />
-                    {formatError.date_start && <p className="text-red-500 text-xs">{formatError.date_start}</p>}
+                    {formatError.title_th && <p className="text-red-500 text-xs">{formatError.title_th}</p>}
                 </div>
                 <div>
-                    <Label htmlFor="date_end">วันที่สิ้นสุด</Label>
+                    <Label htmlFor="title_en">ชื่อกิจกรรม (ภาษาอังกฤษ)</Label>
                     <Input
-                        type="date"
-                        name="date_end"
-                        value={formData.date_end}
-                        onChange={(e) => handleDateChange('date_end', e.target.value)}
+                        type="text"
+                        name="title_en"
+                        value={formData.title_en}
+                        onChange={handleInputChange}
+                        placeholder="กรอกชื่อ Event"
                     />
-                    {formatError.date_end && <p className="text-red-500 text-xs">{formatError.date_end}</p>}
+                    {formatError.title_en && <p className="text-red-500 text-xs">{formatError.title_en}</p>}
                 </div>
-            </div>
-            <div>
-                <Label htmlFor="image">อัปโหลดรูปหน้าปก</Label>
-                <Input
-                    type="file"
-                    name="image"
-                    onChange={(e) => handleFileChange(e.target.files[0])}
-                    accept="image/*"
-                />
-                {formatError.image && <p className="text-red-500 text-xs">{formatError.image}</p>}
-            </div>
-            <Button type="submit" className="w-full mt-4" disabled={loading} > {loading ? "กำลังโหลด..." : "สร้างกิจกรรม"}</Button>
-        </form>
+
+                <div>
+                    <Label htmlFor="location">สถานที่จัดกิจกรรม</Label>
+                    <Input
+                        type="text"
+                        name="location"
+                        value={formData.location}
+                        onChange={handleInputChange}
+                        placeholder="กรอก สถานที่จัดกิจกรรม"
+                    />
+                    {formatError.location && <p className="text-red-500 text-xs">{formatError.location}</p>}
+                </div>
+                <div>
+                    <Label htmlFor="description_th">รายละเอียดกิจกรรม</Label>
+                    <Textarea
+                        name="description_th"
+                        value={formData.description_th}
+                        onChange={handleInputChange}
+                        placeholder="กรอกรายละเอียด Event"
+                    />
+                    {formatError.description_th && <p className="text-red-500 text-xs">{formatError.description_th}</p>}
+                </div>
+                <div>
+                    <Label htmlFor="description_en">รายละเอียดกิจกรรม (ภาษาอังกฤษ)</Label>
+                    <Textarea
+                        name="description_en"
+                        value={formData.description_en}
+                        onChange={handleInputChange}
+                        placeholder="กรอกรายละเอียด Event"
+                    />
+                    {formatError.description_en && <p className="text-red-500 text-xs">{formatError.description_en}</p>}
+                </div>
+                <div className="grid grid-cols-2 gap-4">
+                    <div>
+                        <Label htmlFor="date_start">วันที่เริ่ม</Label>
+                        <Input
+                            type="date"
+                            name="date_start"
+                            value={formData.date_start}
+                            onChange={(e) => handleDateChange('date_start', e.target.value)}
+                        />
+                        {formatError.date_start && <p className="text-red-500 text-xs">{formatError.date_start}</p>}
+                    </div>
+                    <div>
+                        <Label htmlFor="date_end">วันที่สิ้นสุด</Label>
+                        <Input
+                            type="date"
+                            name="date_end"
+                            value={formData.date_end}
+                            onChange={(e) => handleDateChange('date_end', e.target.value)}
+                        />
+                        {formatError.date_end && <p className="text-red-500 text-xs">{formatError.date_end}</p>}
+                    </div>
+                </div>
+                <div>
+                    <Label htmlFor="image">อัปโหลดรูปหน้าปก</Label>
+                    <Input
+                        type="file"
+                        name="image"
+                        onChange={(e) => handleFileChange(e.target.files[0])}
+                        accept="image/*"
+                    />
+                    {formatError.image && <p className="text-red-500 text-xs">{formatError.image}</p>}
+                </div>
+                <Button type="submit" className="w-full mt-4" disabled={loading} > {loading ? "กำลังโหลด..." : "สร้างกิจกรรม"}</Button>
+            </form>
+            {loading &&
+                <div className="modal fixed inset-0 bg-gray-800 bg-opacity-50 flex justify-center items-center z-50">
+                    <div className=" rounded-lg shadow-lg flex flex-col items-center">
+                        <div className="flex flex-col items-center space-y-2">
+                            <Lottie animationData={AnimationDownload} loop={true} className="w-1/2 h-1/2" />
+                            <Button
+                                className="w-1/5 items-center  my-5 group relative overflow-hidden bg-black text-white font-bold py-3 px-6 rounded-full shadow-lg transition-all duration-300 ease-out hover:scale-105 hover:shadow-xl active:scale-95">
+                                <span className="relative z-10 flex items-center justify-center gap-3">
+                                    <span className="text-lg">Loading...</span>
+                                </span>
+                                <span className="absolute inset-0 z-0 bg-black opacity-0 transition-opacity duration-300 ease-out group-hover:opacity-100" />
+                            </Button>
+                        </div>
+                    </div>
+                </div>
+            }
+        </div>
     )
 }
