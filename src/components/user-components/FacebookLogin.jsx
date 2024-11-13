524915e50db937a126cc90cc3f0b7bda1307ed6f
Sarunyamk <Sarunya46mk@gmail.com>
2024-11-13 11:39:00 +0700

    edit

--- src/components/user-components/FacebookLogin.jsx
+++ src/components/user-components/FacebookLogin.jsx
@@ -1,15 +1,37 @@
 import React from 'react';
 import FacebookLogin from 'react-facebook-login';
 import { loginFacebook } from "../../apis/AuthApi";
+import { useNavigate } from 'react-router-dom';
+import useAuthStore from '@/src/stores/AuthStore';
+
 
 function FacebookLoginButton() {
+    const navigate = useNavigate();
+    const actionLoginFacebook = useAuthStore((state) => state.actionLoginFacebook);
+
     const handleFacebookLogin = async (response) => {
         const { accessToken, userID } = response;
 
         if (accessToken && userID) {
             try {
                 const backendResponse = await loginFacebook(accessToken, userID);
-                console.log('Login Success:', backendResponse.data);
+                if (backendResponse.data && backendResponse.data.user) {
+
+                }
+                const user = backendResponse.data.user;
+                const token = backendResponse.data.token;
+                console.log(user, "user respon")
+                console.log(token, "user respon")
+
+                if (user.role === 'USER' || user.role === 'VOLUNTEER') {
+                    actionLoginFacebook(token, user);
+                    navigate('/');
+
+                }
+                else if (user.role === 'ADMIN') {
+                    actionLoginFacebook(token, user);
+                    navigate('/admin');
+                }
             } catch (error) {
                 console.error('Login Failed:', error);
             }
